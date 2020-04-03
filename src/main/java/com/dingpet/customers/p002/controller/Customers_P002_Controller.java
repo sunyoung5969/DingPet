4d3478c1ffd2caef114ea2d24f9f4b6dbc02da4e
SYW <sunyoung5969@gmail.com>
2020-04-03 10:05:31 +0900

    0403

--- src/main/java/com/dingpet/customers/p002/controller/Customers_P002_Controller.java
+++ src/main/java/com/dingpet/customers/p002/controller/Customers_P002_Controller.java
@@ -1,29 +1,10 @@
+
 package com.dingpet.customers.p002.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.dingpet.customers.p002.service.Customers_P002_Service;
+public interface Customers_P002_Controller {
 
-import lombok.AllArgsConstructor;
-
-@RequestMapping("/customers/p002/*")
-@AllArgsConstructor
-@Controller
-public class Customers_P002_Controller {
-
-	private Customers_P002_Service service;
-	
-	@RequestMapping("/lostid")
-	public void lostid(Model model) {
-		
-		System.out.println("���ɤĤ����������ɞ����������ɱQ�����̿�����");
-		
-		int test2 = service.getTotal();
-		
-		model.addAttribute("test2", test2);
-		
-	}
+	public void lostid(Model model);
 	
 }
