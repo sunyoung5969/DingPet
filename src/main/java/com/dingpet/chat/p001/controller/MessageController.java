bb1483d6a8e3f4ed389709a1171ba92f1383dc9a
Sun <Sun@DESKTOP-F5BJNOR>
2020-05-14 17:00:43 +0900

    commit

--- src/main/java/com/dingpet/chat/p001/controller/MessageController.java
+++ src/main/java/com/dingpet/chat/p001/controller/MessageController.java
@@ -48,10 +48,7 @@ public class MessageController {
     	
     	Date date = new Date();
     	SimpleDateFormat form = new SimpleDateFormat("yy. MM. dd a hh:mm");
-
     	System.out.println(form.format(date));
-    	
-    	
     	
     	message.setMessage(message.getWriter() + "님이 입장하셨습니다");
         message.setMessageType("System message"); 
